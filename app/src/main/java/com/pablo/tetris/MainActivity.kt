961ec6d1350247647e5a4b31e4a4e9e7bd1ae633
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-04-19 14:25:02 +0200

    feat(initial): added inital pages and colors

--- app/src/main/java/com/pablo/tetris/MainActivity.kt
+++ app/src/main/java/com/pablo/tetris/MainActivity.kt
@@ -2,10 +2,24 @@ package com.pablo.tetris
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        hideStatusBar()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    private fun hideStatusBar() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controllerCompat = WindowInsetsControllerCompat(window, window.decorView)
+        controllerCompat.hide(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.navigationBars())
+        controllerCompat.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    }
+
 }
