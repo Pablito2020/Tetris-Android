51f2df0fca853d764f296043b59cdb5c4fc9b952
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-04-26 17:55:48 +0200

    feat(game): implement grid

---
+++ app/src/main/java/com/pablo/tetris/Game.kt
@@ -0,0 +1,88 @@
+package com.pablo.tetris
+
+import GameFacade
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import board.Cell
+import com.pablo.tetris.databinding.ActivityGameBinding
+import game.GameCell
+
+
+class Game : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityGameBinding
+    private lateinit var gameFacade: GameFacade
+    private lateinit var adapter: GameAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        gameFacade = GameFacade(ghost = true)
+        gameFacade.start()
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        adapter = GameAdapter(gameFacade.getGrid().flatMap { it.toList() }, this)
+        binding.GameGrid.adapter = adapter
+        setUpButtons()
+    }
+
+    private fun setUpButtons() {
+        binding.DownButton.setOnClickListener(this)
+        binding.LeftButton.setOnClickListener(this)
+        binding.RightButton.setOnClickListener(this)
+        binding.RotateLeft.setOnClickListener(this)
+        binding.RotateRight.setOnClickListener(this)
+    }
+
+    class GameAdapter(var gameCells: List<GameCell>, private val context: Context) : BaseAdapter() {
+
+        override fun getCount(): Int = 10 * 20
+
+        override fun getItem(p0: Int): Any = gameCells[p0]
+
+        override fun getItemId(p0: Int): Long = p0.toLong()
+
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            val view = p1 ?: View.inflate(context, R.layout.grid_item, null)
+            val cell: TextView = view.findViewById(R.id.grid_item)
+            cell.background =
+                AppCompatResources.getDrawable(context, getColorForCell(gameCells[p0]))!!
+            return view
+        }
+
+        private fun getColorForCell(gameCell: GameCell): Int {
+            if (gameCell.isGhostBlockCell) {
+                return R.color.vibrant_gray
+            }
+            return when (gameCell.cell) {
+                Cell.EMPTY -> R.color.black
+                Cell.I_BLOCK -> R.color.vibrant_yellow
+                Cell.J_BLOCK -> R.color.vibrant_blue
+                Cell.L_BLOCK -> R.color.vibrant_cyan
+                Cell.SQUARE_BLOCK -> R.color.vibrant_green
+                Cell.S_BLOCK -> R.color.vibrant_red
+                Cell.T_BLOCK -> R.color.vibrant_magenta
+                Cell.Z_BLOCK -> R.color.vibrant_white
+            }
+        }
+
+    }
+
+    override fun onClick(p0: View) {
+        when (p0.id) {
+            binding.DownButton.id -> gameFacade.down()
+            binding.LeftButton.id -> gameFacade.left()
+            binding.RightButton.id -> gameFacade.right()
+            binding.RotateLeft.id -> gameFacade.rotateLeft()
+            binding.RotateRight.id -> gameFacade.rotateRight()
+        }
+        adapter.gameCells = gameFacade.getGrid().flatMap { it.toList() }
+        adapter.notifyDataSetChanged()
+    }
+
+}
--- app/src/main/java/com/pablo/tetris/MainActivity.kt
+++ app/src/main/java/com/pablo/tetris/MainActivity.kt
@@ -1,21 +1,25 @@
 package com.pablo.tetris
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.pablo.tetris.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
+    //TODO: On resume hide status bar too!
     override fun onCreate(savedInstanceState: Bundle?) {
         hideStatusBar()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.startButton.setOnClickListener(this)
     }
 
     private fun hideStatusBar() {
@@ -24,6 +28,18 @@ class MainActivity : AppCompatActivity() {
         controllerCompat.hide(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.navigationBars())
         controllerCompat.systemBarsBehavior =
             WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    }
+
+    override fun onClick(p0: View) {
+        when(p0.id) {
+            binding.startButton.id -> startGameActivity()
+            else -> TODO("More buttons need to go here")
+        }
+    }
+
+    private fun startGameActivity() {
+        val game = Intent(this, Game::class.java)
+        startActivity(game)
     }
 
 }
