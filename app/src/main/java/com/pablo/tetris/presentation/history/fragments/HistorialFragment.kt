bffe426132bc753c2747af189aad7320f9aafacb
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-05-28 10:07:18 +0200

    refactor(historial): extract function

--- app/src/main/java/com/pablo/tetris/presentation/history/fragments/HistorialFragment.kt
+++ app/src/main/java/com/pablo/tetris/presentation/history/fragments/HistorialFragment.kt
@@ -17,6 +17,7 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.pablo.tetris.R
+import com.pablo.tetris.infra.database.Player
 import com.pablo.tetris.infra.database.PlayerApplication
 import com.pablo.tetris.presentation.history.CurrentGameLogActivity
 import com.pablo.tetris.presentation.history.model.HistoryViewModel
@@ -28,7 +29,6 @@ import kotlin.properties.Delegates
 class HistorialFragment : Fragment() {
 
     private lateinit var historyViewModel: HistoryViewModel
-
     private var hasLogFragment by Delegates.notNull<Boolean>()
     private lateinit var adapter: PlayerAdapter
 
@@ -45,36 +45,21 @@ class HistorialFragment : Fragment() {
 
     private fun setUpRecyclerView() {
         historyViewModel.executeQuery()
+        setUpAdapterRecyclerView()
+        setUpRecyclerViewUi()
+        setUpRecyclerViewObserver()
+    }
+
+    private fun setUpAdapterRecyclerView() {
         adapter = PlayerAdapter(
             historyViewModel.getPlayers(),
-            deleteButtonListener = {
-                androidx.appcompat.app.AlertDialog.Builder(requireContext())
-                    .setTitle(R.string.warning)
-                    .setMessage(requireContext().getString(R.string.delete_game_warning, it.name))
-                    .setNegativeButton(R.string.cancel) { _, _ -> Toast.makeText(context, R.string.deletion_canceled, Toast.LENGTH_SHORT).show() }
-                    .setPositiveButton(R.string.ok) { _, _ -> historyViewModel.deletePlayer(it); Toast.makeText(context, R.string.deleted_player_game, Toast.LENGTH_SHORT).show() }
-                    .create()
-                    .show()
-            },
-            itemClickListener = {
-                historyViewModel.showLogForPlayer(it)
-                if (!hasLogFragment) {
-                    val intent = Intent(requireContext().applicationContext, CurrentGameLogActivity::class.java)
-                    requireContext().startActivity(intent)
-                }
-            },
-            itemLongCLickListener = { view, player ->
-                val popupMenu = PopupMenu(requireContext(), view)
-                popupMenu.menuInflater.inflate(R.menu.popup_menu, popupMenu.menu)
-                popupMenu.setOnMenuItemClickListener {
-                        val autoComplete = requireView().findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
-                        autoComplete.setText(player.name)
-                        autoComplete.clearFocus()
-                        true
-                }
-                popupMenu.show()
-            }
+            deleteButtonListener = { deletePlayer(it) },
+            itemClickListener = { showLogForPlayer(it) },
+            itemLongCLickListener = { view, player -> showMenuForFiltering(view, player) }
         )
+    }
+
+    private fun setUpRecyclerViewUi() {
         val recyclerViewHistory = requireView().findViewById<RecyclerView>(R.id.recyclerViewHistory)
         recyclerViewHistory.adapter = adapter
         val manager = LinearLayoutManager(context)
@@ -85,9 +70,11 @@ class HistorialFragment : Fragment() {
                 manager.orientation
             )
         )
+    }
+
+    private fun setUpRecyclerViewObserver() {
         historyViewModel.updatedDataBase.observe(viewLifecycleOwner) {
-            val autoComplete =
-                requireView().findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+            val autoComplete = requireView().findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
             val names = autoComplete.text.toString()
             adapter.players = historyViewModel.getAutoCompleteResult(names)
             adapter.notifyDataSetChanged()
@@ -131,6 +118,36 @@ class HistorialFragment : Fragment() {
 
     companion object BundleConstants {
         const val HAS_LOG_FRAGMENT = "hasLogFragment"
+    }
+
+    private fun deletePlayer(player: Player) {
+        androidx.appcompat.app.AlertDialog.Builder(requireContext())
+            .setTitle(R.string.warning)
+            .setMessage(requireContext().getString(R.string.delete_game_warning, player.name))
+            .setNegativeButton(R.string.cancel) { _, _ -> Toast.makeText(context, R.string.deletion_canceled, Toast.LENGTH_SHORT).show() }
+            .setPositiveButton(R.string.ok) { _, _ -> historyViewModel.deletePlayer(player); Toast.makeText(context, R.string.deleted_player_game, Toast.LENGTH_SHORT).show() }
+            .create()
+            .show()
+    }
+
+    private fun showLogForPlayer(player: Player) {
+        historyViewModel.showLogForPlayer(player)
+        if (!hasLogFragment) {
+            val intent = Intent(requireContext().applicationContext, CurrentGameLogActivity::class.java)
+            requireContext().startActivity(intent)
+        }
+    }
+
+    private fun showMenuForFiltering(root: View, player: Player) {
+        val popupMenu = PopupMenu(requireContext(), root)
+        popupMenu.menuInflater.inflate(R.menu.popup_menu, popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener {
+            val autoComplete = requireView().findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+            autoComplete.setText(player.name)
+            autoComplete.clearFocus()
+            true
+        }
+        popupMenu.show()
     }
 
 }
